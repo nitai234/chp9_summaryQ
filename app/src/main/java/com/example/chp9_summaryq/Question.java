88794cae870252438ad4247923518a4961302418
USER <nitairynar@gmail.com>
2023-08-10 14:19:04 +0300

    Initial commit

---
+++ app/src/main/java/com/example/chp9_summaryq/Question.java
@@ -0,0 +1,77 @@
+package com.example.chp9_summaryq;
+
+public class Question
+{
+    private String question, ans1, ans2, ans3, ans4;
+    private int correct;
+
+    public Question(String question, String ans1, String ans2, String ans3, String ans4, int correct)
+    {
+        this.question = question;
+        this.ans1 = ans1;
+        this.ans2 = ans2;
+        this.ans3 =ans3;
+        this.ans4 = ans4;
+        this.correct = correct;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "question='" + question + '\'' +
+                ", ans1='" + ans1 + '\'' +
+                ", ans2='" + ans2 + '\'' +
+                ", ans3='" + ans3 + '\'' +
+                ", ans4='" + ans4 + '\'' +
+                ", correct=" + correct +
+                '}';
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public void setAns1(String ans1) {
+        this.ans1 = ans1;
+    }
+
+    public void setAns2(String ans2) {
+        this.ans2 = ans2;
+    }
+
+    public void setAns3(String ans3) {
+        this.ans3 = ans3;
+    }
+
+    public void setAns4(String ans4) {
+        this.ans4 = ans4;
+    }
+
+    public void setCorrect(int correct) {
+        this.correct = correct;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAns1() {
+        return ans1;
+    }
+
+    public String getAns2() {
+        return ans2;
+    }
+
+    public String getAns3() {
+        return ans3;
+    }
+
+    public String getAns4() {
+        return ans4;
+    }
+
+    public int getCorrect() {
+        return correct;
+    }
+}
---
+++ app/src/main/java/com/example/chp9_summaryq/Questions.java
@@ -0,0 +1,61 @@
+package com.example.chp9_summaryq;
+
+import java.util.ArrayList;
+
+public class Questions
+{
+    private int current = 0;
+    private ArrayList <Question> questions;
+
+    public Questions()
+    {
+        questions = new ArrayList<Question>();
+        Question q1 = new Question("23 + 9", "34", "32", "45", "29", 2);
+        Question q2 = new Question("123 * 34", "5008", "4632", "4182", "3965", 3);
+        Question q3 = new Question("מהי העיר בירה של עירק", "בגדאד", "טהרן", "ארביל", "פריז", 1);
+        Question q4 = new Question("מה פירוש המילה summry", "קיצי", "מסלול", "מחבט", "סיכום", 4);
+        Question q5 = new Question("כמה ספרים ג.ק.רולינג הוציאה", "7", "10", "8", "11", 2);
+        Question q6 = new Question("באיזה שנה הומצא הוויז", "2006", "2000", "2011", "2008", 1);
+        Question q7 = new Question("1 / 0", "1", "0", "12356", "אין פתרון", 4);
+        Question q8 = new Question("מי הכי מגניב בעולם", "נתאי ריינר", "לא ידוע", "אף אחד", "אני", 1);
+        Question q9 = new Question("איפה נמצא הקוטב הצפוני", "בשמיים", "בדרום", "בצפון", "הוא לא קיים", 3);
+        Question q10 = new Question("כמה שעות אריות ישנים ביום", "12", "20", "6", "16", 2);
+        Question q11 = new Question("295 / 5", "59", "61", "53", "49", 1);
+        Question q12 = new Question("כמה יבשות יש", "6", "7", "8", "9", 2);
+        Question q13 = new Question("1+1", "2", "2", "4", "11",2);
+        Question q14 = new Question("מהו האיבר המיותר בגוף", "מוח", "לבלב", "ציפורן של הזרת", "תוספתן", 4);
+        Question q15 = new Question("כמה שאלות יש בשאלון הזה", "20", "16", "15", "30", 3);
+
+        questions.add(q1);
+        questions.add(q2);
+        questions.add(q3);
+        questions.add(q4);
+        questions.add(q5);
+        questions.add(q6);
+        questions.add(q7);
+        questions.add(q8);
+        questions.add(q9);
+        questions.add(q10);
+        questions.add(q11);
+        questions.add(q12);
+        questions.add(q13);
+        questions.add(q14);
+        questions.add(q15);
+    }
+
+    public void setCurrent(int current) {
+        this.current = current;
+    }
+
+    public void setQuestions(ArrayList<Question> questions) {
+        this.questions = questions;
+    }
+
+    public int getCurrent() {
+        return current;
+    }
+
+    public ArrayList<Question> getQuestions() {
+        return questions;
+    }
+}
